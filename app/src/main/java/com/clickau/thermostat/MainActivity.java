3e95cedb2096b270915d20ca12e10c914044a2c2
Clickau <firmabugiii@gmail.com>
2020-02-11 16:55:49 +0200

    Display the current activity/fragment's name in the toolbar

--- app/src/main/java/com/clickau/thermostat/MainActivity.java
+++ app/src/main/java/com/clickau/thermostat/MainActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -17,12 +18,14 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.main_activity_toolbar);
+        toolbar = findViewById(R.id.main_activity_toolbar);
         setSupportActionBar(toolbar);
 
         SharedPreferences pref = getSharedPreferences("firebase_credentials", Context.MODE_PRIVATE);
@@ -42,8 +45,13 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
 
         // open the dashboard when the app is opened, but not when the phone is rotated
-        if (savedInstanceState == null)
+        if (savedInstanceState == null) {
             bottomNavigationView.setSelectedItemId(R.id.nav_dashboard);
+            ActionBar actionBar = getSupportActionBar();
+            // if we set the title from the toolbar, it won't work when the application is first started
+            if (actionBar != null)
+                getSupportActionBar().setTitle(R.string.bottom_nav_dashboard);
+        }
     }
 
     @Override
@@ -51,12 +59,15 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         switch (menuItem.getItemId())
         {
             case R.id.nav_dashboard:
+                toolbar.setTitle(R.string.bottom_nav_dashboard);
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new DashboardFragment()).commit();
                 break;
             case R.id.nav_schedules:
+                toolbar.setTitle(R.string.bottom_nav_schedules);
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SchedulesFragment()).commit();
                 break;
             case R.id.nav_more:
+                toolbar.setTitle(R.string.bottom_nav_more);
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MoreFragment()).commit();
                 break;
         }
